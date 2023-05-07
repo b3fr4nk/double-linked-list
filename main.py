e05f227e4162d5f64fa332be16562e1bbed4ad9b
Chris Wood <wchristopherwood912@gmail.com>
2023-05-07 10:58:05 -0700

    fixed import error

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from doublyLinkedList import DoublyLinkedList
+
 dll = DoublyLinkedList()
 dll.append(10)
 print(dll.head.data)
 dll.append(12)
-print(dll.head.next)
+print(dll.head.next.data)
 print(dll.head.next.previous.data)
